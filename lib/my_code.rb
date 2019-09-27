c4aa560ae0c100def7e7f3b88f354e7557c8dd45
Peter Sampson <petersampson010@gmail.com>
2019-09-27 13:48:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,52 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each do |index|
+    new_array.push(yield(index))
+  end 
+  return new_array
+end 
+
+def reduce(source_array, starting_point = 0)
+  total = starting_point
+  source_array.each do |i|
+    yield(i, starting_point)
+  end 
+  return yield 
+end
+
+
+
+
+    
+#def reduce_to_all_true(source_array)
+ # source_array.all? {|i| i} 
+#end 
+
+#def reduce_to_any_true(source_array)
+  #source_array.any? {|i| i}
+#end  
+  
+  
+  
+#def reduce_to_total(source_array, starting_point = 0)
+ # total = starting_point
+  #source_array.each do |i| 
+   # total+=i 
+  #end 
+  #return total 
+#end 
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
